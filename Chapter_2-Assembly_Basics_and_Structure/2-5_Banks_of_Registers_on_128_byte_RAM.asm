67852083be261ae162bd02f93c77a25ecb1e9244
a coder <docdaodoi1@gmail.com>
2023-09-28 16:35:47 +0700

    Good to learn a new things that change my Way of Viewing, and There are a lot of things that you dont know that you dont know

---
+++ Chapter_2-Assembly_Basics_and_Structure/2-5_Banks_of_Registers_on_128_byte_RAM.asm
@@ -0,0 +1,8 @@
+; To go clearly, so good
+; The content of RAM locations (that contain the bank of registers)
+    MOV R0, #95H
+    MOV R1, #85H
+    MOV R2, #3FH
+    MOV R7, #63H
+    MOV R5, #12H
+	END
