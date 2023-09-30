d58e4a271067964642b794c0333df06ae5ddb0ed
a coder <docdaodoi1@gmail.com>
2023-09-30 21:29:46 +0700

    till Finish

---
+++ Chapter_2-Assembly_Basics_and_Structure/2-10_relocate_another_section_of_RAM_for_Stack.asm
@@ -0,0 +1,11 @@
+; Till the last, tired for now, need sleep
+        MOV SP, #5FH
+        
+        MOV R2, #25H
+        MOV R1, #12H
+        MOV R4, #0F3H
+
+        PUSH 2
+        PUSH 1
+        PUSH 4
+        END
