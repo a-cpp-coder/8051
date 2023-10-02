52bffebaa0c75a686abd15de6746a27e057750d3
a coder <docdaodoi1@gmail.com>
2023-10-02 15:36:08 +0700

    just add all for now, you should read the book to know the foundation

--- Chapter_3-Jump,Loop,_and_Call_instructions/3-1_a_Loop_with_conditional_Jump.asm
+++ Chapter_3-Jump,Loop,_and_Call_instructions/3-1_a_Loop_with_conditional_Jump.asm
@@ -2,8 +2,8 @@
 
 ; This program adds value 3 to the ACC ten times
         MOV A, #0           ; A = 0, clear ACC
-        MOV R2, #FF         ; load counter R2 = 10
-AGAIN:  ADD A, #03
+        MOV R2, #0FFH         ; load counter R2 = 10
+AGAIN:  ADD A, #01
         DJNZ R2, AGAIN      ; repeat until R2 = 0
         MOV R5, A           ; save A in R5
         END
